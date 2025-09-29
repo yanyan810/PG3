50aed06e5b6ba9e43e55c62e4742e6780d13df3a
yanyan0315 <k024g0064@g.neec.ac.jp>
2025-09-29 11:06:49 +0900

    プロジェクト ファイルを追加します。

---
+++ 00-01/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+	SetConsoleOutputCP(65001);
+	//表示おおお
+
+	char str[] = "あ";
+
+	printf("%s\n",str);
+	return 0;
+}
---
+++ PG3/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("World!\n");
+	return 0;
+}
